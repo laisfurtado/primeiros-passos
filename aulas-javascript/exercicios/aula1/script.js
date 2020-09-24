829ff5da7c94219c407e0e4e957da55243451397
Lais Furtado <laisfurtado@gmail.com>
2020-09-24 20:51:20 -0300

    Comitando resolução do exercicio1 de JS

---
+++ aulas-javascript/exercicios/aula1/script.js
@@ -0,0 +1,13 @@
+
+function checarSePodeDirigir () { 
+    let idade = prompt ('Qual sua idade?');
+    
+    if (idade > 18) { 
+        alert ('Pode dirigir!')
+     }  else {
+         alert ('Ainda não pode dirigir!')
+     }
+    }
+
+    
+checarSePodeDirigir (17); 
